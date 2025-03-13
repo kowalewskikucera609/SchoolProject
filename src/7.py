8e3639ba4dc7d681d8e7660585c48e43546b910f
kowalewskikucera609 <kowalewskikucera609@gmail.com>
2025-03-13 11:28:13 +0000

    Add 7.py

---
+++ src/7.py
@@ -0,0 +1,11 @@
+import random
+def random_python_code():
+return random.choice([
+"print('Hello, world!')",
+'x = 5',
+'y = "string"',
+'z = True',
+'a = [1, 2, 3]',
+'b = (4, 5, 6)',
+'c = {7: 8, 9: 10}'
+])
