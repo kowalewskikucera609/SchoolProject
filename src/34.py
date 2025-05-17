39c7c3593870ea7dd749cb79ddd665516608699d
kowalewskikucera609 <kowalewskikucera609@gmail.com>
2025-05-17 08:58:22 +0000

    Add 34.py

---
+++ src/34.py
@@ -0,0 +1,12 @@
+# This is just an example Python code. Feel free to modify it as needed.
+import os
+
+def print_hello():
+    """
+    Prints "Hello, World!" to the console.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    # You can use this line of code for testing purposes if you want to see how your script works
+    print_hello()
